674763db2135d78b596aff8c7befcf5440150827
Jeanne Bouaziz <jeannebouaziz@hotmail.fr>
2019-02-13 13:09:33 +0100

    Rails Yelp MVP ongoing : models done, controller done, view for show and index done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,15 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
