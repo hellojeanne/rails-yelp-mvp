2bc70b05c362daf6ee3f145dc78a67d0ec8021d0
Jeanne Bouaziz <jeannebouaziz@hotmail.fr>
2019-02-13 15:53:21 +0100

    Navbar done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'reviews/create'
   get 'reviews/new'
+  root to: 'restaurants#index'
   resources :restaurants do
     resources :reviews, only: [ :new, :create ]
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
